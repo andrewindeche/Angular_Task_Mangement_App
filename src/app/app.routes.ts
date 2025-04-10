976572c68863b9e99c9624f865d102ecfacb99e7
andrewindeche <indecheandrew@gmail.com>
2025-04-10 21:31:04 +0300

    Add route for tasks id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { TaskdetailsComponent } from './taskdetails/taskdetails.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/tasks', pathMatch: 'full' },  
-    { path: 'tasks', component: TasktableComponent, pathMatch: 'full' },  
+    { path: 'tasks', component: TasktableComponent, pathMatch: 'full' },
+    { path: 'tasks/:id', component: TasktableComponent, pathMatch: 'full' }, 
     { path: 'detail/new', component: TaskdetailsComponent, pathMatch: 'full' },  
     { path: 'detail/:id', component: TaskdetailsComponent } 
 ];
